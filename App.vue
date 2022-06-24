0dac39e733acd2638fd18814186a44af4d2837f2
Emiria486 <87558503+Emiria486@users.noreply.github.com>
2022-06-24 14:12:26 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<style lang="less">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing:border-box;
+}
+a{
+  text-decoration: none;
+  color: black;
+  cursor: pointer;
+}
+</style>
