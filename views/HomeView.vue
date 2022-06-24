6e3c20bd615eb3e1e95c88d1597a3f170a6c81ba
Emiria486 <87558503+Emiria486@users.noreply.github.com>
2022-06-24 14:12:05 +0800

    Add files via upload

---
+++ views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+  <TopNavVue></TopNavVue>
+  <SceneChangeVue></SceneChangeVue>
+  <RelatedProductsVue></RelatedProductsVue>
+  <HomeFooterVue></HomeFooterVue>
+  </div>
+</template>
+
+<script>
+import TopNavVue from '@/components/home/TopNav.vue'
+import SceneChangeVue from '@/components/home/SceneChange.vue'
+import RelatedProductsVue from '@/components/home/RelatedProducts.vue'
+import HomeFooterVue from '@/components/home/HomeFooter.vue'
+export default {
+    name: "HomeView",
+    components: {TopNavVue,SceneChangeVue,RelatedProductsVue,HomeFooterVue,}
+}
+</script>
